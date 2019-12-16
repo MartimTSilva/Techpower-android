15ba41471639780310634e34f6fc5c178c70145e
Micael Rodrigues <micaelr95@outlook.com>
2019-12-16 12:19:32 +0000

    Create database

--- app/src/main/java/com/example/techpower/helpers/StoreDBHelper.java
+++ app/src/main/java/com/example/techpower/helpers/StoreDBHelper.java
@@ -13,6 +13,20 @@ public class StoreDBHelper extends SQLiteOpenHelper {
 
     private final SQLiteDatabase database;
 
+    // Table Category
+    private static final String CATEGORY_TABLE_NAME = "category";
+    private static final String CATEGORY_ID = "id";
+    private static final String CATEGORY_NAME = "name";
+    private static final String CATEGORY_PARENT = "parent_id";
+
+    // Table Products
+    private static final String PRODUCT_TABLE_NAME = "product";
+    private static final String PRODUCT_ID = "id";
+    private static final String PRODUCT_NAME = "name";
+    private static final String PRODUCT_DESCRIPTION = "description";
+    private static final String PRODUCT_PRICE = "price";
+    private static final String PRODUCT_CATEGORY = "category_id";
+
     public StoreDBHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DB_VERSION);
 
@@ -21,12 +35,30 @@ public class StoreDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // TODO: Create database SQL
+        // Category create table SQL
+        String createTableCategory = "CREATE TABLE " + CATEGORY_TABLE_NAME + "(" +
+                CATEGORY_ID + " INTEGER PRIMARY KEY, " +
+                CATEGORY_NAME + " TEXT NOT NULL, " +
+                "FOREIGN KEY (" + CATEGORY_PARENT + ") REFERENCES " + CATEGORY_TABLE_NAME + "(" + CATEGORY_ID + "));";
+
+        // Product create table SQL
+        String createTableProduct = "CREATE TABLE " + PRODUCT_TABLE_NAME + "(" +
+                PRODUCT_ID + " INTEGER PRIMARY KEY, " +
+                PRODUCT_NAME + " TEXT NOT NULL, " +
+                PRODUCT_DESCRIPTION + " TEXT NOT NULL, " +
+                PRODUCT_PRICE + " INTEGER NOT NULL, " +
+                "FOREIGN KEY (" + PRODUCT_CATEGORY + ") REFERENCES " + CATEGORY_TABLE_NAME + "(" + CATEGORY_ID + "));";
+
+        // Create tables
+        database.execSQL(createTableCategory);
+        database.execSQL(createTableProduct);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // TODO: Drop database
+        database.execSQL("DROP TABLE IF EXISTS " + PRODUCT_TABLE_NAME);
+        database.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE_NAME);
+        this.onCreate(db);
     }
 
     // TODO: Create crud
