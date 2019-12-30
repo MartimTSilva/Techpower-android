a47c1de433971152c09914ab0da985051a79af5a
micaelr95 <micaelr95@outlook.pt>
2019-12-30 03:20:38 +0000

    Add dynamic drawer. Only show login and signup if user is not logged in and show logout if user is logged in

--- app/src/main/java/com/example/techpower/MainActivity.java
+++ app/src/main/java/com/example/techpower/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -31,6 +34,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = findViewById(R.id.nav_view);
         drawer = findViewById(R.id.drawer);
 
+        Menu menu = navigationView.getMenu();
+
+        // Add account submenu
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.app_preferences), MODE_PRIVATE);
+        Menu accountSubMenu = menu.addSubMenu(Menu.NONE, Menu.NONE, 1,"Account");
+        if (preferences.getString("authkey", null) == null) {
+            accountSubMenu.add(Menu.NONE, R.string.nav_login, Menu.NONE, R.string.nav_login);
+            accountSubMenu.add(Menu.NONE, R.string.nav_signup, Menu.NONE, R.string.nav_signup);
+        } else {
+            accountSubMenu.add(Menu.NONE, R.string.nav_logout, Menu.NONE, R.string.nav_logout);
+        }
+        navigationView.invalidate();
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         toggle.syncState();
         drawer.addDrawerListener(toggle);
@@ -49,14 +65,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
         switch (menuItem.getItemId()) {
-            case R.id.nav_login:
+            case R.string.nav_login:
                 Intent login_intent = new Intent(this, LoginActivity.class);
                 startActivity(login_intent);
                 break;
 
-            case R.id.nav_signup:
+            case R.string.nav_signup:
                 Intent signup_intent = new Intent(this, SignUpActivity.class);
                 startActivity(signup_intent);
+                break;
+
+            case R.string.nav_logout:
+                // TODO: Remove user data
                 break;
 
             case R.id.nav_settings:
