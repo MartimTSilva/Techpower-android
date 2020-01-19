ff80fae9b46250c650669448853819ff0476348b
micaelr95 <micaelr95@outlook.pt>
2020-01-19 23:52:40 +0000

    Fix verify if total is 0

--- app/src/main/java/com/example/techpower/CartActivity.java
+++ app/src/main/java/com/example/techpower/CartActivity.java
@@ -50,7 +50,7 @@ public class CartActivity extends AppCompatActivity {
         btn_checkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!total.equals("0.00")) {
+                if (!total.equals("0.00") && !total.equals("0,00")) {
                     btn_checkout.setEnabled(true);
                     Intent intent = new Intent(getApplicationContext(), CheckoutActivity.class);
                     startActivity(intent);
